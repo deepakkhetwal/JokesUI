0ace84c9d02fe30c0ff3edaf7158ccbdc7c8c917
deepakkhetwal <deepakkhetwal@gmail.com>
2014-08-14 01:02:10 -0400

    commited for admin review

--- js/controllers/admn/review-controllers.js
+++ js/controllers/admn/review-controllers.js
@@ -1,12 +1,14 @@
 var reviewControllers = angular.module('reviewControllers',[]);
+
 reviewControllers.controller('reviewIndexCtrl',['$scope', 'reviewService', function($scope, reviewService)
 {
 	$scope.jokesList = reviewService.GetJokeList.query();
+	$scope.SaveReview = function(id){
+		
+		var reviewObj = $.grep($scope.jokesList.jokes, function (e) { return e._id == id; });
+		
+		var joke = JSON.stringify({id : reviewObj[0]._id ,description : reviewObj[0].description, isReviewed : reviewObj[0].is_reviewed, userEmail : reviewObj[0].user_email, jokeCategory: reviewObj[0].joke_category  });
+		reviewService.UpdateJokeReview.save(joke);
+	};
 }])
 
-reviewControllers.controller('updateReviewCtrl', ['$scope', '$reviewService', function($scope, reviewService)
-{
-	$scope.SaveReview = function(){
-		var joke = JSON.stringify({description : $scope.description,  })
-	}
-}])

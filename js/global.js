e33b1ec4c4323be1b8532f41b5f70a93122f457f
deepakkhetwal <deepakkhetwal@gmail.com>
2014-11-11 09:45:50 -0500

    fix global issue

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 var global = {
     WEBSITE_URL: 'http://jokesforkids.herokuapp.com',
-    WEBAPI_URL : 'http://kejokesapi.herokuapp.com'
-    //WEBAPI_URL : 'http://localhost:3000',
+   // WEBAPI_URL : 'http://kejokesapi.herokuapp.com'
+    WEBAPI_URL : 'http://kejokesapi.herokuapp.com',
     FILE_UPLOAD_URL: 'https://elasticbeanstalk-us-west-2-780260980507.s3.amazonaws.com/'
     
 }

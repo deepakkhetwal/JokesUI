7dbc88e35b1b396eb04bf11adf373c7a5b218d46
deepak khetwal <deepakkhetwal@gmail.com>
2015-12-08 09:13:28 -0500

    angularjs upgrade
    angularjs upgrade

--- js/modules/home-module.js
+++ js/modules/home-module.js
@@ -1,26 +1,3 @@
-var            homeModule = angular.module('homeModule',['ngRoute','jokesMainControllers', 'jokesMainService', 'reviewControllers', 'reviewService'
-	, 'feedbacksMainControllers', 'feedbacksMainService','jokesMostLikedControllers'
-	]);
-homeModule.config(['$routeProvider', function($routeProvider){
-
-	$routeProvider.
-		when('/', {templateUrl: 'views/jokes_main/jokes-list-partial.html', controller: 'jokesMainCtrl'})
-		.when('/jokes/mostliked',{templateUrl: 'views/jokes_main/jokes-most-liked-partial.html', controller: 'jokesMostLikedCtrl'})
-		.when('/jokes/create', {templateUrl: 'views/jokes_main/jokes-post-partial.html', controller:'jokesPostCtrl'})
-		.when('/admn/review', {templateUrl: 'views/admn/review/review-list-partial.html', controller:'reviewIndexCtrl'})
-		.when('/contact', {templateUrl: 'views/contact.html'})
-		.when('/about',{templateUrl: 'views/about.html'})
-		.when('/jokes/uploadmessage', {templateUrl: 'views/upload/jokes-video-upload-message.html'})
-		.when('/feedbacks/create', {templateUrl: 'views/feedback/feedbacks-post-partial.html', controller: 'feedbacksPostCtrl'});
-
-}])
-.factory('$exceptionHandler', function () {
-    return function (exception, cause) {
-    	alert(exception.message);
-      exception.message += ' (caused by "' + cause + '")';
-     
-    };
-});
 (function(){
 	angular
 		.module('homeModule',['ngRoute',
@@ -31,5 +8,23 @@ homeModule.config(['$routeProvider', function($routeProvider){
 			'feedbacksMainControllers',
 			 'feedbacksMainServiceModule',
 			 'jokesMostLikedControllers'
-	]);
+		])
+		.config(['$routeProvider', function($routeProvider){
+			$routeProvider.
+			when('/', {templateUrl: 'views/jokes_main/jokes-list-partial.html', controller: 'jokesMainCtrl'})
+			.when('/jokes/mostliked',{templateUrl: 'views/jokes_main/jokes-most-liked-partial.html', controller: 'jokesMostLikedCtrl'})
+			.when('/jokes/create', {templateUrl: 'views/jokes_main/jokes-post-partial.html', controller:'jokesPostCtrl'})
+			.when('/admn/review', {templateUrl: 'views/admn/review/review-list-partial.html', controller:'reviewIndexCtrl'})
+			.when('/contact', {templateUrl: 'views/contact.html'})
+			.when('/about',{templateUrl: 'views/about.html'})
+			.when('/jokes/uploadmessage', {templateUrl: 'views/upload/jokes-video-upload-message.html'})
+			.when('/feedbacks/create', {templateUrl: 'views/feedback/feedbacks-post-partial.html', controller: 'feedbacksPostCtrl'});
+			}])
+		.factory('$exceptionHandler', function () {
+    return function (exception, cause) {
+    	alert(exception.message);
+      exception.message += ' (caused by "' + cause + '")';
+     	};
+		});
+
 });

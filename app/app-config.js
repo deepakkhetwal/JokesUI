acaeb7537bd182b2abc42880a6adc842ea9f7b1b
deepak khetwal <deepakkhetwal@gmail.com>
2017-02-27 22:25:16 -0500

    Rewrite to angular

--- app/app-config.js
+++
@@ -1,9 +0,0 @@
-var appConfig = appConfig || {};
-appConfig = {
-   WEBSITE_URL: 'http://jokesforkids.herokuapp.com',
-  	//WEBSITE_URL: 'http://localhost:8000',
-    WEBAPI_URL : 'http://kejokesapi.herokuapp.com',
-   // WEBAPI_URL : 'http://localhost:3000',
-    FILE_UPLOAD_URL: 'https://elasticbeanstalk-us-west-2-780260980507.s3.amazonaws.com/'
-    
-};
